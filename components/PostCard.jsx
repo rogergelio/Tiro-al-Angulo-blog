8675fc1ee2325413bcc0710d89c8f86a243cf9ed
rogergelio <87195114+rogergelio@users.noreply.github.com>
2022-08-03 00:37:29 -0500

    color fix

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -14,7 +14,7 @@ const PostCard = ({ post }) => {
       </div>
       <h1
         className="transition duration-700 text-center mb-8 cursor:pointer
-      hover:text-pink-600 text-3xl font-semibold
+      hover:text-green-600 text-3xl font-semibold
       "
       >
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
