505246e04fea82b0b1ca7855f753f075a1261336
rogergelio <87195114+rogergelio@users.noreply.github.com>
2022-08-03 00:15:39 -0500

    final draft

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
 import { AdjacentPosts } from '../../sections';
