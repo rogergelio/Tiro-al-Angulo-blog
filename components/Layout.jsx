8be7b7dc3c4f77fa547ba840752ba6ae0f4701d0
rogergelio <87195114+rogergelio@users.noreply.github.com>
2022-08-31 19:13:50 -0500

    ads

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,10 @@ import { Header } from "./";
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
+      <Header>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9425943405163248"
+     crossorigin="anonymous"></script>
+      </ Header>
       {children}
     </>
   );
