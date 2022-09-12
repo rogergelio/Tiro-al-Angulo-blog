18ac283482495acddee8e551dd864b3b7a88779f
rogergelio <87195114+rogergelio@users.noreply.github.com>
2022-09-12 10:18:13 -0500

    logging articles

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home({ posts }) {
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
+        {console.log(posts)}
           {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
