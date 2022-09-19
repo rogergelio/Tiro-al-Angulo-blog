28b41791ec453c437efeeeef62332e85c921bc68
rogergelio <87195114+rogergelio@users.noreply.github.com>
2022-09-19 04:19:00 -0500

    animation

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-import { FeaturedPosts } from '../sections/index'
-import { PostCard, Categories, PostWidget, FeaturedPostCard } from '../components';
-import { getPosts, getPostsTwo, getFeaturedPost } from '../services';
+import { FeaturedPosts } from "../sections/index";
+import {
+  PostCard,
+  Categories,
+  PostWidget,
+  FeaturedPostCard,
+} from "../components";
+import { getPosts, getPostsTwo, getFeaturedPost } from "../services";
 
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1 animate-in slide-in-from-left duration-700">
+        <div className="lg:col-span-8 col-span-1 animate-in slide-in-from-left duration-700 animate-out slide-out-to-right duration-300">
           {console.log(Date.now())}
-        {console.log(posts)}
+          {console.log(posts)}
           {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
