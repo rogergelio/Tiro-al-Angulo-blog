793db1e38374cc23dc526035a10d7dc99744e33c
Manchitas_11 <rogergeliotorres@gmail.com>
2022-11-08 11:52:02 -0600

    us message aded

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -24,7 +24,7 @@ const PostDetail = ({ post }) => {
       case 'heading-three':
         return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
       case 'paragraph':
-        return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+        return <p key={index} className="mb-8 text-base">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
       case 'heading-four':
         return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
       case 'image':
--- components/Us.jsx
+++ components/Us.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import Image from "next/image";
 import fab from "../public/fab4.jpeg";
 
-var textArray = ['Somos los fans.', 'Somos los amantes del fut.', 'Somos los aficionados.']
+var textArray = [
+  "Somos los fans.",
+  "Somos los amantes del fut.",
+  "Somos los aficionados.",
+];
 
 class Us extends Component {
   constructor() {
@@ -27,12 +31,41 @@ class Us extends Component {
     return (
       <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-          <div className="lg:col-span-8 col-span-1 animate-in slide-in-from-left duration-700">
-            <div className="aboutUsContainer">
-              <h1 className="usText text-white text-5xl font-bold ">
+          <div className="lg:col-span-12 col-span-1 animate-in slide-in-from-left duration-700">
+            <div className="aboutUsContainer text-center bg-black/15 shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+              <h1 className="usText text-white text-5xl font-bold mb-4">
                 {textThatChanges}
               </h1>
-              <h2 className="text-1xl"></h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Tiro al Ángulo comenzó como el podcast de 4 amigos de la universidad.
+              </h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Igual que nosotros, ese podcast está tratando de crecer. 
+              </h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Queremos ser la comunidad de fútbol más chingona de todo el mundo.
+              </h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Llena de fans.
+              </h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Llena de aficionados. 
+              </h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Llena de gente como ustedes. 
+              </h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Así que comenten, peleen, y no tengan miedo de
+                expresar sus opiniones pamboleras sobre este increíble deporte,
+                que para eso estamos. 
+              </h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Juntos, podemos ser la nueva voz del fútbol. 
+              </h2>
+              <h2 className="text-2xl text-white bg-grey mb-1">
+                Sin más que decir, esto es Tiro al Ángulo,
+                esperamos hablar con ustedes pronto.
+              </h2>
             </div>
           </div>
         </div>
