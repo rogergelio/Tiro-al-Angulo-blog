89a3d50d776d18f75d9cd8ab53ec33c0334f5c68
Manchitas_11 <rogergeliotorres@gmail.com>
2022-11-08 12:44:50 -0600

    basic structure ready

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { getCategories } from "../services";
+import {Navbar } from '../components'
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
@@ -14,30 +15,7 @@ const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-4">
       <div className="border-b w-full inline-block border-white-400 py-8">
-        <div className="md:float-left block">
-          <Link href="/">
-            <div>
-              <span className="cursor-pointer font-bold text-4xl text-white">
-                Tiro al Ángulo
-              </span>
-              <h1 className="cursor-pointer font-semi text-s text-white">
-                El blog 100% pambolero
-              </h1>
-            </div>
-          </Link>
-        </div>
-        <div className="headerFlexbox md:float-right mt-4">
-          <Link href="/nav/us">
-            <h1 className="cursor-pointer font-semi text-2xl text-white">
-              Nosotros
-            </h1>
-          </Link>
-          <Link href="/nav/results">
-            <h1 className="cursor-pointer font-semi text-2xl text-white">
-              Resultados
-            </h1>
-          </Link>
-        </div>
+        <Navbar/>
       </div>
     </div>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import NavItem from "./NavItem";
 
+const MENU_LIST = [
+  { text: "Nosotros", href: "/nav/us" },
+  { text: "About Us", href: "/nav/results" },
+];
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
 
-export default Navbar
+  return (
+    <header>
+      <nav className={`nav`}>
+      <Link href="/">
+            <div>
+              <span className="cursor-pointer font-bold text-4xl text-white">
+                Tiro al Ángulo
+              </span>
+              <h1 className="cursor-pointer font-semi text-s text-white">
+                El blog 100% pambolero
+              </h1>
+            </div>
+          </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
