9faf6ef2754eb6a00a59a0d503cd019dda0e7b0e
Manchitas_11 <rogergeliotorres@gmail.com>
2022-11-14 11:27:45 -0600

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
