41e3c092dced0379b81bd60c0faba71353b3e317
Manchitas_11 <rogergeliotorres@gmail.com>
2022-11-27 14:46:57 -0600

    aa

--- components/NavItem.jsx
+++ components/NavItem.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 const NavItem = ({ text, href, active }) => {
   return (
-    <Link href={href}>
+    <Link legacyBehavior href={href}>
       <a className={`nav__link`}>{text}</a>
     </Link>
   );
