37c85c8fb0125c2587dded1c0985d118552beb2d
Manchitas_11 <rogergeliotorres@gmail.com>
2022-12-25 11:42:09 -0600

    removed google ads

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,8 +5,6 @@ const Layout = ({ children }) => {
   return (
     
     <>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9425943405163248"
-     crossOrigin="anonymous"></script>
       <Header>
       </Header>
       {children}
