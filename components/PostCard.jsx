284c414c9c00090e18a77a4fe9617ce69d51928f
Manchitas_11 <rogergeliotorres@gmail.com>
2022-12-25 11:34:15 -0600

    image size fix

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,11 +6,13 @@ import Image from "next/image"
 const PostCard = ({ post }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
-      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-        <img
+      <div className="relative overflow-hidden mb-4">
+        <Image
           src={post.featuredImage.url}
           alt={post.title}
-          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+          className="object-top absolute w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+          width="788px"
+          height="320px"
         />
       </div>
       <h1
--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -29,7 +29,7 @@ const PostDetail = ({ post }) => {
         return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
       case 'image':
         return (
-          <img
+          <Image
             key={index}
             alt={obj.title}
             height={obj.height}
