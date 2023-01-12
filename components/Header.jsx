cc9274f4e59429da46781265e1d6a7651c067734
Manchitas_11 <rogergeliotorres@gmail.com>
2023-01-12 11:35:06 -0600

    ads added

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,6 +13,8 @@ const Header = () => {
 
   return (
     <div className="container mx-auto px-2 max-h-15v bg-neutral-900">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9886424723448101"
+     crossorigin="anonymous"></script>
       <div className="w-full inline-block ">
         <Navbar/>
       </div>
